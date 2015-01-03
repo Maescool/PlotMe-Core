ed00b5ea9a4b4622af0bd38096c4f2d4acdf43a0
CaLxCyMru <me@callum-morris.com>
2015-01-03 15:24:08 +0000

    Fix silly typo

--- src/main/java/com/worldcretornica/plotme_core/commands/CmdClear.java
+++ src/main/java/com/worldcretornica/plotme_core/commands/CmdClear.java
@@ -82,7 +82,7 @@ public class CmdClear extends PlotCommand {
                             } else {
                                 Date timeLeft = new Date(plot.getLastPlotClear().getTime() + plugin.getServerBridge().getConfig().getInt("PlotClearTime") - now.getTime());
                                 DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-                                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c" + C("MsgThisPlot") + "(" + id + ") " + C("MsgNotAllowedClearYet").replace("{data}", formatter.format(timeLeft))));
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&c" + C("MsgThisPlot") + "(" + id + ") " + C("MsgNotAllowedClearYet").replace("{date}", formatter.format(timeLeft))));
                             }
                         } else {
                             player.sendMessage("§c" + C("MsgThisPlot") + "(" + id + ") " + C("MsgNotYoursNotAllowedClear"));
