d3bfa4d71a34741debc6945127d696a4eaef6e09
Pieter Maes <maescool@gmail.com>
2015-01-24 21:11:49 +0100

    Fix pagination

--- src/main/java/com/worldcretornica/plotme_core/commands/CmdShowHelp.java
+++ src/main/java/com/worldcretornica/plotme_core/commands/CmdShowHelp.java
@@ -129,7 +129,9 @@ public class CmdShowHelp extends PlotCommand {
 
         player.sendMessage("§c ---==§b" + C("HelpTitle") + " " + page + "/" + maxPage + "§c==--- ");
 
-        for (int ctr = (((page - 1) * 4)-1); ctr < (page * 4) && ctr < allowed_commands.size(); ctr++) {
+        if (page == 1) player.sendMessage("Number of commands available: "+allowed_commands.size());
+
+        for (int ctr = (((page - 1) * 4)); ctr < (page * 4) && ctr < allowed_commands.size(); ctr++) {
             String allowedCommand = allowed_commands.get(ctr);
 
             if ("limit".equalsIgnoreCase(allowedCommand)) {
